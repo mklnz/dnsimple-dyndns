dbf71cfdac114b23eb40099f9f6e0e39589e79d1
Michael Shi <michael@shi.co.nz>
2014-08-17 02:33:56 +1000

    Server secret added

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require_relative 'lib/dns_updater'
 
+set :bind, '0.0.0.0'
+
+config = YAML.load_file(File.join(__dir__, "config/config.yml"))
+
 get '/update/:ip' do
-  DnsUpdater.update(params[:ip])
+  if params[:s] == config["server_secret"]
+    DnsUpdater.update(params[:ip])
+    "UPDATED"
+  end
 end
